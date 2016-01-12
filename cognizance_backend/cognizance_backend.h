0ffb1f43e7f6af498c580a807424dfdc99714ef7
Rashid Khan <rashood.khan@gmail.com>
2016-01-12 19:07:43 -0300

    Initial commit with the Objective-Git Library added

---
+++ cognizance_backend/cognizance_backend.h
@@ -0,0 +1,19 @@
+//
+//  cognizance_backend.h
+//  cognizance_backend
+//
+//  Created by Rashid Khan on 1/12/16.
+//  Copyright © 2016 Cognizance. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for cognizance_backend.
+FOUNDATION_EXPORT double cognizance_backendVersionNumber;
+
+//! Project version string for cognizance_backend.
+FOUNDATION_EXPORT const unsigned char cognizance_backendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cognizance_backend/PublicHeader.h>
+
+
