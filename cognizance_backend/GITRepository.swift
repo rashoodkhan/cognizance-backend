a9d72b640ac63c206ebcdf3fff712f0466804326
Rashid Khan <rashood.khan@gmail.com>
2016-01-12 21:47:28 -0300

    Added the bridging header to get ObjectiveGit working on Swift

---
+++ cognizance_backend/GITRepository.swift
@@ -0,0 +1,14 @@
+//
+//  GITRepository.swift
+//  cognizance_backend
+//
+//  Created by Rashid Khan on 1/12/16.
+//  Copyright © 2016 Cognizance. All rights reserved.
+//
+
+import Foundation
+import ObjectiveGit
+
+class GITRepository {
+	var repositoryName: String = "";
+}
---
+++ cognizance_backend/ObjectiveGit-BridgingHeader.m
@@ -0,0 +1,9 @@
+//
+//  ObjectiveGit-BridgingHeader.m
+//  cognizance_backend
+//
+//  Created by Rashid Khan on 1/12/16.
+//  Copyright © 2016 Cognizance. All rights reserved.
+//
+
+#import <ObjectiveGit/ObjectiveGit.h>
